df12d3279de005a224fd3ad154d713474fd56649
vdias38 <victor.dias@meumobi.com>
2020-02-20 14:49:39 +0000

    feat: go to a specific page when clicking on a notification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { FcmService } from '../core/push-notification/fcm.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   items: Observable<any[]>;
   token: string;
 
   constructor(
-    db: AngularFirestore,
-    private fcmService: FcmService
+    private db: AngularFirestore,
+    private fcmService: FcmService,
+    private router: Router
   ) {
     this.items = db.collection('items').valueChanges();
+  }
+
+  ngOnInit() {
+    this.fcmService.listenToNotifications().subscribe(
+      data => {
+        console.log('FCM message: ', data);
+        if (data.tap) {
+          if (data.id && data.type) {
+            this.router.navigate([`${data.type}/detail/${data.id}`]);
+          } else if (data.url) {
+            this.router.navigate([data.url]);
+          }
+        }
+    });
   }
 
   async getToken(): Promise<void> {
